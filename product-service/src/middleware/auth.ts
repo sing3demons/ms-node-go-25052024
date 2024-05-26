52b53ebb131a78f46bffd7e3c1436172c37113d7
sing3demons <sing3demons@live.com>
2024-05-26 10:58:29 +0700

    chore: Update auth-service client HTTP requests, logger, and user routes

---
+++ product-service/src/middleware/auth.ts
@@ -0,0 +1,60 @@
+import axios, { AxiosResponse, AxiosRequestConfig, RawAxiosRequestHeaders } from 'axios'
+import type { Request, Response, NextFunction } from 'express'
+import jwt from 'jsonwebtoken'
+
+type IAuthService = {
+  message: string
+  success: string
+  data: {
+    access_token: string
+  }
+}
+
+type IBody = {
+  access_token: string
+}
+
+type PayloadToken = {
+  sub: string
+  exp: number
+  iat: number
+  email: string
+}
+
+type CustomRequest = Request & { token?: PayloadToken }
+
+export default class AuthService {
+  validateToken = async (req: Request, res: Response, next: NextFunction) => {
+    const authorization = req.headers['authorization']
+    if (!authorization) {
+      res.status(401).send('Unauthorized')
+      return
+    }
+    const access_token = authorization.split(' ')[1]
+
+    const config: AxiosRequestConfig = { headers: { 'Content-Type': 'application/json' } }
+    const data = { access_token: access_token }
+
+    try {
+      const result = await axios.post<IBody, AxiosResponse<IAuthService>, IBody>(
+        'http://localhost:8080/api/v1/auth/verify',
+        data,
+        config
+      )
+
+      if (result.status === 200) {
+        const token = result.data.data.access_token
+
+        const payload = jwt.decode(token) as PayloadToken
+
+        ;(req as CustomRequest).token = payload
+
+        next()
+      } else {
+        res.status(401).send('Unauthorized')
+      }
+    } catch (error) {
+      res.status(401).send('Unauthorized')
+    }
+  }
+}
--- product-service/src/root-route.ts
+++ product-service/src/root-route.ts
@@ -3,12 +3,14 @@ import { IRoute, TypeRoute } from './core/my-route'
 import { ProductRouter } from './product/product.route'
 import { LoggerType } from './core/logger'
 import { type MongoClient } from 'mongodb'
+import AuthService from './middleware/auth'
 
 export default function registerRoute(myRoute: IRoute = new TypeRoute(), client: MongoClient, logger: LoggerType) {
   const router = Router()
+  const { validateToken } = new AuthService()
 
   router.get('/healthz', (_req, res) => res.status(200).json('OK'))
 
-  router.use('/products', new ProductRouter(myRoute, logger, client).register())
+  router.use('/products', validateToken, new ProductRouter(myRoute, logger, client).register())
   return router
 }
